6aa403fdcbe0184be5718f9f5b785cadec5933c2
KamilVlcek <kamil@biomed.cas.cz>
2015-04-23 14:39:14 +0200

    RAW data - anotace - pridal jsem jednoduchy graf casu anotaci jako svisle cary

--- anotace.m
+++ anotace.m
@@ -4,9 +4,16 @@ function [  ] = anotace( H, tabs, zac)
 
 disp(['celkova delka v sekundach ' num2str(H.records) ',' ...
     'od ' datestr(tabs(1),'dd-mmm-yyyy HH:MM:SS.FFF') ', do '  datestr(tabs(end),'dd-mmm-yyyy HH:MM:SS.FFF') ]);
+figure('Name','Anotace');
 for a = zac: size(H.annotation.starttime)
    disp([ num2str(a) ' ' strtrim(H.annotation.event{a}) ' ... ' num2str( H.annotation.starttime(a)) ...
        ' s ... ' num2str( H.annotation.starttime(a)*H.samplerate(1))]);
+   cas = H.annotation.starttime(a);
+   %cas0 = iff(a == zac,H.annotation.starttime(1),H.annotation.starttime(a-1));
+   plot([cas cas],[0 1]); %vykreslim cas anotaci do grafu
+   hold on;
 end
+
+
 end
 
