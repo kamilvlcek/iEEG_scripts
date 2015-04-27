f535a24fb0c65398e2272f9bc0b14dca57f8363e
KamilVlcek <kamil@biomed.cas.cz>
2015-04-27 16:13:50 +0200

    HILBERT - spectra.m - signifikantni rozdily vuci predstimulu
    - pomoci WilcoxA
    - prehodil jsem indexy v polich allHH allPP aj:  freq, cas, epochy
    - drobne upravy vypisu

--- spektra.m
+++ spektra.m
@@ -6,29 +6,29 @@ freq = 10:10:150;
 
 wsize = floor(wsize_sec * EEG.srate);
 woverlap = 2; %pro jine hodnoty nechapu pocet hodnot, ktere vraci spectrogram
-allPP = zeros( [ size(EEG.data,3) size(freq,2)  floor(size(EEG.data,2)/wsize*woverlap-1) ] ); %pocet hodnot spektrogramu zkousim odhadnout 
-allHH = zeros( [ size(EEG.data,3) size(freq,2)-1  size(EEG.data,2)] ); %epochy frekvence cas
+allPP = zeros( [  size(freq,2)  floor(size(EEG.data,2)/wsize*woverlap-1) size(EEG.data,3) ] ); %pocet hodnot spektrogramu zkousim odhadnout 
+allHH = zeros( [  size(freq,2)-1  size(EEG.data,2) size(EEG.data,3)] ); % frekvence cas epochy
 
 %vypocet prumerne sily ve frekvencnich pasmech
-ffPP = zeros( [size(EEG.data,3) size(freq,2)  ]); %prumerny spectrogram pro kazdou epochu a frekvenci
-ffHH = zeros( [size(EEG.data,3) size(freq,2)-1]); %prumerny hilbert za kazdou epochu a frekvenci
+ffPP = zeros( [ size(freq,2)  size(EEG.data,3) ]); %prumerny spectrogram pro kazdou frekvenci a epochu
+ffHH = zeros( [ size(freq,2)-1 size(EEG.data,3)]); %prumerny hilbert za kazdou frekvenci a epochu
 for epocha = 1: size(EEG.data,3);
     %spectrogram
     [~,~,~,P]=spectrogram(double(EEG.data(ch,:,epocha)),wsize,wsize/woverlap,freq,EEG.srate,'yaxis');
     PP = 10*log10(abs(P));
     %hilbert
     for f = 1:numel(freq)
-        ffPP(epocha,f)=mean(PP(f,:));
+        ffPP(f,epocha)=mean(PP(f,:));
         if f < numel(freq) %hilbert je vzdy od do, takze pro posledni neni definovany
-            ffHH(epocha,f)=mean(hilbertJirka(double(EEG.data(ch,:,epocha)),freq(f), freq(f+1),EEG.srate));
+            ffHH(f,epocha)=mean(hilbertJirka(double(EEG.data(ch,:,epocha)),freq(f), freq(f+1),EEG.srate));
         end
     end
 end
-nPP = mean(ffPP,1); %prumer spectrogram pro kazdou frekvenci pres vsechny epochy
-nHH = mean(ffHH,1); %prumer hilbert pro kazdou frekvenci pres vsechny epochy
+nPP = mean(ffPP,2); %prumer spectrogram pro kazdou frekvenci pres vsechny epochy
+nHH = mean(ffHH,2); %prumer hilbert pro kazdou frekvenci pres vsechny epochy
 
 
-fprintf('ep ');
+fprintf('epocha: ');
 for epocha = 1: size(EEG.data,3);
     %spectrogram
     [S,F,T,P]=spectrogram(double(EEG.data(ch,:,epocha)),wsize,wsize/woverlap,freq,EEG.srate,'yaxis');
@@ -36,18 +36,21 @@ for epocha = 1: size(EEG.data,3);
     for f = 1:numel(freq)
         PP(f,:) = PP(f,:) ./ nPP(f); 
     end
-    allPP(epocha,:,:)=PP; %frekvence=radky, cas=sloupce
+    allPP(:,:,epocha)=PP; %frekvence=radky, cas=sloupce
     
     %hilbert
     for f = 1:numel(freq)-1
         H = hilbertJirka(double(EEG.data(ch,:,epocha)),freq(f), freq(f+1),EEG.srate);
         H = H ./ nHH(f);
-        allHH(epocha,f,:)=H;
+        allHH(f,:,epocha)=H;
     end
     
     fprintf('%i ',epocha);
 end
-meanPP = squeeze(mean(allPP,1));
+fprintf('\n'); %konec radku
+
+%grafy
+meanPP = squeeze(mean(allPP,3)); %prumer pres vsechny epochy
 figure('Name','prumerny spektrogram');
 imagesc(T,F,meanPP);
 axis xy;
@@ -56,7 +59,7 @@ line([eventlatency eventlatency], [freq(1) freq(end)],'LineWidth',2,'Color','bla
 colorbar;
 caxis([-0,2.5]);
 
-meanHH = squeeze(mean(allHH,1));
+meanHH = squeeze(mean(allHH,3)); %prumer pres vsechny epochy
 figure('Name','prumerny hilbert');
 imagesc(T,F,meanHH);
 axis xy;
@@ -66,4 +69,14 @@ line([eventlatency eventlatency], [freq(1) freq(end)],'LineWidth',2,'Color','bla
 colorbar;
 caxis([-0,2.5]);
 
-disp('hotovo');
+%statistika
+C = mean(allHH(:,1:EEG.event(1,1).latency-1,:),2); %prumer pres cas 
+fdr = 1;
+W = WilcoxA(allHH,C);
+figure('Name','W map hilbert');
+imagesc(T,F, 1-W,[ iff(fdr,0.95,0.99) 1]);%mapa, od p>0.05 bude modra barva 
+colorbar;
+axis xy;
+
+
+
