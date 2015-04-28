5b5e889df8a571c8021619cd55c4976824f1a4dd
KamilVlcek <kamil@biomed.cas.cz>
2015-04-28 15:15:29 +0200

    RAW data - anotace - graf jen pokud anotace existuji

--- anotace.m
+++ anotace.m
@@ -2,14 +2,18 @@ function [  ] = anotace( H, tabs, zac)
 %ANOTACE vypise vsechny anotace v zaznamu spolu s jejich casem
 %   H, zacatek 1 nebo vice
 
+if nargin < 3, zac=1; end %kdyz neuvedu zacatek, jede se od prvni anotace
 disp(['celkova delka v sekundach ' num2str(H.records) ',' ...
     'od ' datestr(tabs(1),'dd-mmm-yyyy HH:MM:SS.FFF') ', do '  datestr(tabs(end),'dd-mmm-yyyy HH:MM:SS.FFF') ]);
-figure('Name','Anotace');
+
+
 for a = zac: size(H.annotation.starttime)
+    
    disp([ num2str(a) ' ' strtrim(H.annotation.event{a}) ' ... ' num2str( H.annotation.starttime(a)) ...
        ' s ... ' num2str( H.annotation.starttime(a)*H.samplerate(1))]);
    cas = H.annotation.starttime(a);
    %cas0 = iff(a == zac,H.annotation.starttime(1),H.annotation.starttime(a-1));
+   if a==zac, figure('Name','Anotace'); end
    plot([cas cas],[0 1]); %vykreslim cas anotaci do grafu
    hold on;
 end
--- anotacenajdi.m
+++ anotacenajdi.m
@@ -7,6 +7,6 @@ files = dir(fullfile(adresar, '*.mat'));
 for f = 1:numel(files)
     load([ adresar files(f).name],'header','tabs');
     disp(['*** ' files(f).name]);
-    anotace(header,tabs,1);
+    anotace(header,tabs);
     clear header d tabs fs;
 end
