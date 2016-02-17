de130636546f52284a39d506295264fc53d84562
KamilVlcek <kamil@biomed.cas.cz>
2016-02-17 16:58:13 +0100

    RAWDATA - anonymizace dat
    - prepise header.pacientID hodnotou id
    - projel jsem ti vsechny svoje stazena data

---
+++ anonymize.m
@@ -0,0 +1,36 @@
+function []=anonymize(adresar,id)
+% ANONYMIZE prepise header.pacientID hodnotou id
+% napriklad anonymize('D:\eeg\motol\pacienti\VT8\','p79');
+% vse ulozi znova pod jmenem s pismenem X na konci
+
+
+files = dir(fullfile(adresar, '*.mat'));
+
+for f = 1:numel(files)
+    disp(['nacitam ' files(f).name]);
+    load([ adresar files(f).name]);
+    newfilename = strrep(files(f).name, '.mat', '_X.mat');
+    if exist('header','var')
+        if isfield(header,'patientID')
+            disp(['puvodni id: ' header.patientID]);
+        else
+            disp('prazdne id ');
+        end
+        header.patientID = id;
+        save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3');    
+        disp(['ulozeno s header: ' newfilename]);
+    elseif exist('H','var')
+        if isfield(H,'patientID')
+            disp(['puvodni id: ' H.patientID]);
+        else
+            disp('prazdne id ');
+        end
+        H.patientID = id;
+        save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3')
+        disp(['ulozeno s H: ' newfilename]);
+    else
+        disp(['zadny header, neulozeno: ' files(f).name]);
+    end
+    clearvars -except adresar id files f; %smazu vsechny promenne
+end
+disp('hotovo');
