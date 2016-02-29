f70b055163612d0dc71b15a0ccc04546c71bbf83
KamilVlcek <kamil@biomed.cas.cz>
2016-02-29 17:44:29 +0100

    RAWDATA - vylepseni skriptu dataview
    - vraci timestamp zacatku obrazku
    - kresli jen jeden obrazek, neceka na klavesu
    - misto delky se zadava konec obrazku
    - komentare

--- dataview.m
+++ dataview.m
@@ -1,28 +1,34 @@
-function [  ] = dataview(d,H,start,delka, channel)
+function [ tsZAC ] = dataview(d,H,tabs,start,konec, channel)
 %DATAVIEW zobrazi synchronizacni puls s polu s anotacema
 %   pracuje s datama EEG z Motola
 %   delka i start jsou v sekundach
+%   nepovinny channel je cislo kanalu se synchronizaci
 
-if nargin<5 %muzu zadat kanal se synchronizaci - 15.4.2015
+if ~exist('channel','var') %muzu zadat kanal se synchronizaci - 15.4.2015
     channel = size(d,2)-2; %synchronizace byva 2 kanaly pred koncem - pred EKG
-end    
-if delka==0, delka = H.records-1; end %23.6.2015 - kdyz neudam delku, zobrazuje cely zaznam
+end  
+if ~exist('konec','var')
+    delka = H.records-1;  %23.6.2015 - kdyz neudam delku, zobrazuje cely zaznam
+else
+    delka = konec - start;
+end
 if start==0, start = 1; end;
 
 figure('Name','Synchronizace');
 
 %procenta = delka/size(d,1)*100;
-for x = start:delka:H.records; %x je index zacatku
-    if x+delka-1 > H.records
-       break; %kdyz uz delkou presahuju konec zaznamu, ukoncim cyklus
-    end
+%for x = start:delka:H.records; %x je index zacatku
+%    if x+delka-1 > H.records
+%       break; %kdyz uz delkou presahuju konec zaznamu, ukoncim cyklus
+%    end
+x = start;
     plot( x:1/H.samplerate(1):x+delka-1/H.samplerate(1),  d(x*H.samplerate(1):(x+delka)*H.samplerate(1)-1,channel));
     axis([x x+delka -3000 3000])
     sekund_zac = x; % cas v sekundach,cisla s desetinnymi teckami se do grafu na osu x nevejdou
     sekund_konec = (x+delka);
     %sekund_delka = delka/H.samplerate(1);
     %set(gca,'XTickLabel',cellstr(int2str( ( sekund_zac : (sekund_delka/10) : sekund_konec)'))');
-    xlabel(['sekundy z ' num2str(H.records)]); 
+    xlabel(['sekundy z ' num2str(H.records) ' s celkove']); 
     zobrazenych = 0;
     for a = 1:numel(H.annotation.starttime)
         if H.annotation.starttime(a)>sekund_zac && H.annotation.starttime(a)<sekund_konec
@@ -32,10 +38,15 @@ for x = start:delka:H.records; %x je index zacatku
             zobrazenych = zobrazenych+1;
         end
     end
-    disp( num2str(x));
-    keyboard; %zastavi a muzu se divat na promenne, pokracuju pomoci return
+    tsZAC = sec2tabs(x,H.samplerate(1),tabs); %timestamp zacatku
+    disp( ['zacatek: ' num2str(x) 's, timestamp: ' datestr(tsZAC,'dd-mmm-yyyy HH:MM:SS.FFF') ', ' num2str(tsZAC,'%11.15f')]);
+   %keyboard; %zastavi a muzu se divat na promenne, pokracuju pomoci return
+%end
+
 end
 
+function [timestamp]=sec2tabs(sec, fs,tabs)
+    timestamp = tabs(sec*fs);
 end
 
 
