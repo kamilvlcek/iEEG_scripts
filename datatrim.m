43de80985340d1ed8f14eb10195d163d13ae3634
Kamil <kamilvlcek@seznam.cz>
2016-03-02 10:34:34 +0100

    RAWDATA - datatrim - prevod na funkci
    - globalni d a tabs kvuli RAMce

--- datatrim.m
+++ datatrim.m
@@ -1,8 +1,12 @@
+function [header] = datatrim(header,ts0,ts1)
 % redukce dat na vybrany test
 % podle ts0 a ts1 - indexy zacatku a konce
 % updatuju pole d, tabs a header
-% since 1.3.2016
+% since 1.3.201
+
+global d tabs; %header nebudu davat globalne, muze se jmenovat pokazde jinak
 
 d = d(ts0:ts1,:);
 tabs = tabs(ts0:ts1,:);
 header.starttime = datestr(tabs(1),'HH:MM:SS.FFF');
+end
