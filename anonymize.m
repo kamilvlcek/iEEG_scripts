d772fd57db744456de79f4fccc1f7d5fffb30227
Kamil <kamilvlcek@seznam.cz>
2016-03-18 21:21:57 +0100

    RAWDATA - anonymizace dat - jeste lepsi verze
    - maze pole recordID i H a nejen v header
    - kdyz se nezmeni id, ani nesmaze recordID tak zbytecne data neuklada
    - vypisuje co se zmenilo.

--- anonymize.m
+++ anonymize.m
@@ -7,30 +7,63 @@ function []=anonymize(adresar,id)
 files = dir(fullfile(adresar, '*.mat'));
 
 for f = 1:numel(files)
-    disp(['nacitam ' files(f).name]);
+    disp(['*** nacitam ' files(f).name ' ***']);
     load([ adresar files(f).name]);
     newfilename = strrep(files(f).name, '.mat', '_X.mat');
+    ulozit = false;
     if exist('header','var')
         if isfield(header,'patientID')
-            disp(['puvodni id: ' header.patientID]);
+            if strcmp(header.patientID,id)
+                disp(['puvodni id totozne s novym: ' header.patientID]);
+            else
+                disp(['puvodni id jine nez nove: ' header.patientID]);
+                header.patientID = id;
+                ulozit = true;
+            end
         else
             disp('prazdne id ');
+            header.patientID = id;
+            ulozit = true;
+        end        
+        if isfield(header,'recordID')
+            header = rmfield(header, 'recordID'); %pokud tam je
+            disp('recordID smazano ');
+            ulozit = true;
         end
-        header.patientID = id;
-        header = rmfield(header, 'recordID'); %pokud tam je
-        disp(['ukladam ' newfilename]);
-        save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3');    
-        disp(['ulozeno s header: ' newfilename]);
+        if ulozit
+            disp(['ukladam ' newfilename]);
+            save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3');    
+            disp(['ulozeno s header: ' newfilename]);
+        else
+            disp(['zadna zmena, neulozeno: ' newfilename]);
+        end        
     elseif exist('H','var')
         if isfield(H,'patientID')
-            disp(['puvodni id: ' H.patientID]);
+            if strcmp(H.patientID,id)
+                disp(['puvodni id totozne s novym: ' H.patientID]);
+            else
+                disp(['puvodni id jine nez nove: ' H.patientID]);
+                H.patientID = id;
+                ulozit = true;
+            end
         else
             disp('prazdne id ');
+            H.patientID = id;
+            ulozit = true;
         end
         H.patientID = id;
-        disp(['ukladam ' newfilename]);
-        save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3')
-        disp(['ulozeno s H: ' newfilename]);
+        if isfield(H,'recordID')
+            H = rmfield(H, 'recordID'); %pokud tam je
+            disp('recordID smazano ');
+            ulozit = true;
+        end
+        if ulozit
+            disp(['ukladam ' newfilename]);
+            save([ adresar newfilename], '-regexp', '^(?!(adresar|id|files|f|newfilename)$).','-v7.3')
+            disp(['ulozeno s H: ' newfilename]);
+        else
+            disp(['zadna zmena, neulozeno: ' newfilename]);
+        end
     else
         disp(['zadny header, neulozeno: ' files(f).name]);
     end
