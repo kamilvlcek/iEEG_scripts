2984176596077ce5b7240f133e7e0e11a1ce8d5b
KamilVlcek <kamil@biomed.cas.cz>
2016-04-13 14:47:32 +0200

    RAWDATA - datatrim - uprava pro nova data s mults
    - nevyzaduje header

--- datatrim.m
+++ datatrim.m
@@ -1,4 +1,4 @@
-function [header] = datatrim(header,ts0,ts1)
+function [header] = datatrim(ts0,ts1,header)
 % redukce dat na vybrany test
 % podle ts0 a ts1 - indexy zacatku a konce
 % updatuju pole d, tabs a header
@@ -8,7 +8,11 @@ global d tabs; %header nebudu davat globalne, muze se jmenovat pokazde jinak
 
 d = d(ts0:ts1,:);
 tabs = tabs(ts0:ts1,:);
-header.starttime = datestr(tabs(1),'HH:MM:SS.FFF');
-header.records = (tabs(end)-tabs(1))*60*24*60; %timestampy jsou ve dnech, jejich rozdil je pocet dnu, ja chci vteriny
-%nemenim anotace, takze ty pak nesedi
+if exist('header','var')
+    header.starttime = datestr(tabs(1),'HH:MM:SS.FFF');
+    header.records = (tabs(end)-tabs(1))*60*24*60; %timestampy jsou ve dnech, jejich rozdil je pocet dnu, ja chci vteriny
+    %nemenim anotace, takze ty pak nesedi
+else
+    header = 0;
+end
 end
