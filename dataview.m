52aed6758fc4d38cd036e81c463a8d5b3d918f2a
KamilVlcek <kamil@biomed.cas.cz>
2016-04-13 14:39:39 +0200

    RAWDATA - dataview - uprava pro nova data s mults
    - anotace jsou volitelny parametr, pokud se vlozi 0 misto nich, nekresli se
    - mults jsou volitelny parametri, pokud se vlozi 0 misto nich, vytvori se jako 1
    - kdyz se nezada start nebo konec, bere se cely zaznam
    - misto H.records se pocita automaticky zaznamvterin
    - kontroluji se pauzy v datech - pokud je hodnot v tabs mene, nez odpovida casu, viz p96
    -- funkce kontrolacasu
    -- vypise se v jakem case jsou a jak dlouhe
    -- program se ukonci, je treba vymezit usek pomoci start a konec

--- dataview.m
+++ dataview.m
@@ -1,4 +1,4 @@
-function [ iSTART,iEND ] = dataview(d,H,tabs,fs, start,konec,  channel)
+function [ iSTART,iEND ] = dataview(d,tabs,fs,mults, start,konec, annotations, channel)
 %DATAVIEW zobrazi synchronizacni puls s polu s anotacema
 %   pracuje s datama EEG z Motola
 %   delka i start jsou v sekundach
@@ -7,39 +7,58 @@ function [ iSTART,iEND ] = dataview(d,H,tabs,fs, start,konec,  channel)
 if ~exist('channel','var') %muzu zadat kanal se synchronizaci - 15.4.2015
     channel = size(d,2)-2; %synchronizace byva 2 kanaly pred koncem - pred EKG
 end  
+if ~exist('start','var')
+    start = 1;
+    iStart = 1; %index v poli tabs
+else
+    iStart = round(start*fs); %index v poli tabs
+end
+zaznamvterin = (tabs(end)-tabs(1))*24*3600; %delka zaznamu ve vterinach
 if ~exist('konec','var')
-    konec = H.records;
-    delka = H.records-start;  %23.6.2015 - kdyz neudam delku, zobrazuje cely zaznam
+    konec = round(zaznamvterin); 
+    delka = round(zaznamvterin) -start;  %23.6.2015 - kdyz neudam delku, zobrazuje cely zaznam
+    iKonec = size(tabs,1);%index v poli tabs
 else
     delka = konec - start;
+    iKonec = min(size(tabs,1),round(konec*fs));%index v poli tabs
+end
+if ~exist('mults','var') || numel(mults)<size(d,2)
+    mults = ones(1,size(d,2));
 end
 if start==0, start = 1; end;
 %fs = H.samplerate(1); %sampling rate
-figure('Name','Synchronizace');
 
+pauzyvdatech = kontrolacasu(tabs(iStart:iKonec),fs);
+if numel(pauzyvdatech) > 0
+    disp('chyby datech mezi zacatkem a koncem, koncim');
+    return;
+end
+
+figure('Name','Synchronizace');
 %procenta = delka/size(d,1)*100;
 %for x = start:delka:H.records; %x je index zacatku
 %    if x+delka-1 > H.records
 %       break; %kdyz uz delkou presahuju konec zaznamu, ukoncim cyklus
 %    end
 x = start;
-    plot( x:1/fs:x+delka-1/fs,  d(x*fs:(x+delka)*fs-1,channel));
+    plot( x:1/fs:x+delka-1/fs,  d(x*fs:(x+delka)*fs-1,channel) .* mults(1,channel));
     axis([x x+delka -3000 3000])
     sekund_zac = x; % cas v sekundach,cisla s desetinnymi teckami se do grafu na osu x nevejdou
     sekund_konec = (x+delka);
     %sekund_delka = delka/fs;
     %set(gca,'XTickLabel',cellstr(int2str( ( sekund_zac : (sekund_delka/10) : sekund_konec)'))');
-    xlabel(['sekundy z ' num2str(H.records) ' s celkove']); 
+    xlabel(['sekundy z ' num2str(zaznamvterin) ' s celkove']); 
     zobrazenych = 0;
-    for a = 1:numel(H.annotation.starttime)
-        if H.annotation.starttime(a)>sekund_zac && H.annotation.starttime(a)<sekund_konec
-            anotace_index = H.annotation.starttime(a);
-            line([anotace_index anotace_index],[-3000 3000],'Color','red');
-            text(anotace_index,2900-zobrazenych*80,H.annotation.event{a},'Color','red');
-            zobrazenych = zobrazenych+1;
+    if exist('annotations','var') && isstruct(annotations);
+        for a = 1:numel(annotations.starttime)
+            if annotations.starttime(a)>sekund_zac && annotations.starttime(a)<sekund_konec
+                anotace_index = annotations.starttime(a);
+                line([anotace_index anotace_index],[-3000 3000],'Color','red');
+                text(anotace_index,2900-zobrazenych*80,annotations.event{a},'Color','red');
+                zobrazenych = zobrazenych+1;
+            end
         end
     end
-    
     iSTART = x*fs; %index zacatku v poli d a tabs
     tsSTART = tabs(iSTART); %timestamp zacatku
     disp( ['zacatek: ' num2str(x) 's, timestamp: ' datestr(tsSTART,'dd-mmm-yyyy HH:MM:SS.FFF') ', iSTART: ' num2str(iSTART)]);
@@ -49,6 +68,16 @@ x = start;
    %keyboard; %zastavi a muzu se divat na promenne, pokracuju pomoci return
 %end
 
+end
+
+function [dlouhe] = kontrolacasu(tabs,fs)
+tabsdiff = tabs(2:end)-tabs(1:end-1);   %rozdily timestampu
+tabsdiff = tabsdiff*24*3600*1000;       %rozdily v ms
+dlouhe = find(tabsdiff> (1/fs*1000*2)); %najit rozdily v casech vetsi nez dvojnasobek vzorkovaci frekvence
+if numel(dlouhe)  > 0
+    disp(['pauzy v datech: ' num2str(numel(dlouhe))]);
+    disp(['prvni pauza: i=' num2str(dlouhe(1)) '=' num2str(dlouhe(1)/fs) 's, timediff=' num2str(tabsdiff(dlouhe(1))) ' ms']);    
+end
 end
 
 
