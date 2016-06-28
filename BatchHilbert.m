427d3fc4c825febd6e18fd9205a647775db0490a
Kamil <kamilvlcek@seznam.cz>
2016-06-28 10:54:46 +0200

    iEEG - upravy BatchHilbert - smycka frekvenci a pacientu
    - udaje o pacientech jsem shrnul do struktury pacienti
    - udaje o ruznych frekvencnich pasmech jsem shrnul do struktury frekvence
    - zatim nelze pro ruzne pacienty pocitat ruzna frekvencni pasma, ale to je asi dobre
    - vsechno probehlo hladce, jen pro interval 1-7Hz po 1 Hz hlasi warning, zatim nevim proc
    Warning: Matrix is close to singular or badly scaled.          Results may be inaccurate. RCOND = 8.209146e-017.
    CHilbert>CHilbert.hilbertJirka at 179

--- BatchHilbert.m
+++ BatchHilbert.m
@@ -1,80 +1,86 @@
-pacients = {  'p079'}; %'p079' 'p083' 'p095' 'p097'
-freq = 10:10:150; freqname = '10-150';
 basedir = 'd:\eeg\motol\pacienti\';
-if sum(ismember(pacients,'p079'))>0
-    disp('p079 Plu VT8');
-    load([basedir 'p079 Plu VT8\VT8_2015-04-09_09-46_001_concat_X_aedist.mat']);
-    load([basedir 'p079 Plu VT8\P79_header.mat']);
-    load([basedir 'p079 Plu VT8\p79_aedist.mat']);
-    E = CHilbert(d,tabs,fs,[],header);
-    clear d;
-    E.GetHHeader(H);
-    E.RejectChannels([47 64 114]);
-    E.ChangeReference('b');
-    E.PasmoFrekvence(freq);
-    E.ExtractEpochs(aedist,[-.2 1.2]);
-    load([basedir 'p079 Plu VT8\aedist RjEpoch.mat']);
-    E.RejectEpochs(RjEpoch);
-    E.Save([ basedir 'p079 Plu VT8\aedist CHilbert' freqname ' Ep']);
-    disp('p079 OK');
-    clear E d tabs fs mults header RjEpoch aedist H ans;
+timewindow = [-.2 1.2];
+
+frekvence = struct;
+f=1;
+frekvence(f).freq = 50:10:120;
+frekvence(f).freqname = '50-120'; %gamma 2
+f=2;
+frekvence(f).freq = 16:3:31;
+frekvence(f).freqname = '16-31'; % beta
+f=3;
+frekvence(f).freq = 7:2:15;
+frekvence(f).freqname = '7-15'; % alpha
+f=4;
+frekvence(f).freq = 1:1:7;
+frekvence(f).freqname = '1-7'; % theta
+f=5;
+frekvence(f).freq = 30:5:50;
+frekvence(f).freqname = '30-50'; % gamma 1
+
+pacienti = struct;
+p = 1;
+pacienti(p).todo = 1;
+pacienti(p).folder = 'p079 Plu VT8';
+pacienti(p).data = 'VT8_2015-04-09_09-46_001_concat_X_aedist.mat';
+pacienti(p).header = 'P79_header.mat';
+pacienti(p).aedist = 'p79_aedist.mat';
+pacienti(p).rjepoch = 'aedist RjEpoch.mat';
+pacienti(p).rjch = [47 64 114]; %#ok<NBR%#ok<MSNU> AK> 
+pacienti(p).frekvence = struct;
+
+p=2;
+pacienti(p).todo = 1;
+pacienti(p).folder = 'p083 Kol VT10';
+pacienti(p).data = 'VT10_2015-05-19_10-00_001_X_aedist.mat';
+pacienti(p).header = 'P83_header.mat';
+pacienti(p).aedist = 'p83_aedist.mat';
+pacienti(p).rjepoch = 'aedist RjEpoch.mat';
+pacienti(p).rjch = [47]; %#ok<NBRAK>
+
+p=3;
+pacienti(p).todo = 1;
+pacienti(p).folder = 'p095 Hav VT11';
+pacienti(p).data = 'VT11_2015-12-15_aedist.mat';
+pacienti(p).header = 'P95_header.mat';
+pacienti(p).aedist = 'p95_aedist.mat';
+pacienti(p).rjepoch = 'aedist rjepoch.mat';
+pacienti(p).rjch = [47]; %#ok<NBRAK>
+
+p=4;
+pacienti(p).todo = 1;
+pacienti(p).folder = 'p097 Nov VT13';
+pacienti(p).data = 'VT13_2016-02-11_09-20_001 aedist.mat';
+pacienti(p).header = 'P97_header.mat';
+pacienti(p).aedist = 'p97_aedist.mat';
+pacienti(p).rjepoch = 'aedist rj epoch.mat';
+pacienti(p).rjch = [47]; %#ok<NBRAK>
+
+
+for f=1:numel(frekvence)    
+    disp(frekvence(f).freqname); 
+    for p = 1:numel(pacienti)
+        disp(pacienti(p).folder);
+        if pacienti(p).todo
+            load([basedir pacienti(p).folder '\' pacienti(p).data]);
+            load([basedir pacienti(p).folder '\' pacienti(p).header]);
+            load([basedir pacienti(p).folder '\' pacienti(p).aedist]);
+            load([basedir pacienti(p).folder '\' pacienti(p).rjepoch]);
+            if ~exist('mults','var'),  mults = []; end
+            if ~exist('header','var'), header = []; end
+            E = CHilbert(d,tabs,fs,mults,header);
+            clear d;
+            E.GetHHeader(H);
+            E.RejectChannels(pacienti(p).rjch);
+            E.ChangeReference('b');
+            E.PasmoFrekvence(frekvence(f).freq);
+            E.ExtractEpochs(aedist,timewindow);        
+            E.RejectEpochs(RjEpoch);
+            E.ResponseSearch(0.1,[0 1 2]); %statistika s klouzavym oknem 100ms
+            E.Save([ basedir pacienti(p).folder '\aedist CHilbert' frekvence(f).freqname ' Ep']);
+            disp([ pacienti(p).folder 'OK']);
+            clear E d tabs fs mults header RjEpoch aedist H ans; 
+        end
+    end
 end
 
-if sum(ismember(pacients,'p083'))>0
-    %freq = 1:2:9; freqname = '1-9';
-    disp('p083 Kol VT10');
-    load([basedir 'p083 Kol VT10\VT10_2015-05-19_10-00_001_X_aedist.mat']);
-    load([basedir 'p083 Kol VT10\P83_header.mat']);
-    load([basedir 'p083 Kol VT10\p83_aedist.mat']);
-    E = CHilbert(d,tabs,fs,[],header);
-    clear d;
-    E.GetHHeader(H);
-    E.RejectChannels([47]);
-    E.ChangeReference('b');
-    E.PasmoFrekvence(freq);
-    E.ExtractEpochs(aedist,[-.2 1.2]);
-    load([basedir 'p083 Kol VT10\aedist RjEpoch.mat']);
-    E.RejectEpochs(RjEpoch);
-    E.Save([ basedir 'p083 Kol VT10\aedist CHilbert' freqname ' Ep']);
-    disp('p083 OK');
-    clear E d tabs fs mults header RjEpoch aedist H ans;
-end
-
-
-if sum(ismember(pacients,'p095'))>0
-    disp('p095 Hav VT11');
-    load([basedir 'p095 Hav VT11\VT11_2015-12-15_aedist.mat']);
-    load([basedir 'p095 Hav VT11\P95_header.mat']);
-    load([basedir 'p095 Hav VT11\p95_aedist.mat']);
-    E = CHilbert(d,tabs,fs,[],header);
-    clear d;
-    E.GetHHeader(H);
-    E.RejectChannels([47]);
-    E.ChangeReference('b');
-    E.PasmoFrekvence(freq);
-    E.ExtractEpochs(aedist,[-.2 1.2]);
-    load([basedir 'p095 Hav VT11\aedist rjepoch.mat']);
-    E.RejectEpochs(RjEpoch);
-    E.Save([basedir 'p095 Hav VT11\aedist CHilbert' freqname ' Ep']);
-    disp('p095 OK');
-    clear E d tabs fs mults header RjEpoch aedist H ans;
-end
-
-if sum(ismember(pacients,'p097'))>0
-    disp('p097 Nov VT13');
-    load([basedir 'p097 Nov VT13\VT13_2016-02-11_09-20_001 aedist.mat']);
-    load([basedir 'p097 Nov VT13\P97_header.mat']);
-    load([basedir 'p097 Nov VT13\p97_aedist.mat']);
-    E = CHilbert(d,tabs,fs,mults);
-    clear d;
-    E.GetHHeader(H);
-    E.RejectChannels([47]);
-    E.ChangeReference('b');
-    E.PasmoFrekvence(freq);
-    E.ExtractEpochs(aedist,[-.2 1.2]);
-    load([basedir 'p097 Nov VT13\aedist rj epoch.mat']);
-    E.RejectEpochs(RjEpoch);
-    E.Save([basedir 'p097 Nov VT13\aedist CHilbert' freqname ' Ep']);
-    disp('p097 OK');
-    clear E d tabs fs mults header RjEpoch aedist H ans;
-end
