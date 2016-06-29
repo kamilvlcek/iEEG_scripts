9752bb1b9fa112532e8b6f1ea5d9a06dd9b54d33
Kamil <kamilvlcek@seznam.cz>
2016-06-29 09:40:02 +0200

    iEEG - HheaderCreate - vytvoreni zkraceneho headeru se jmeny elektrod
    - podle channels.txt, kam mohou byt jeste pridane a oddelene carkou jmena struktu
    - vytvori header ve formatu od jirky hammer
    - je potreba na bipolarni referenci a na zobrazovaji jmen a labelovani kanalu

---
+++ HheaderCreate.m
@@ -0,0 +1,38 @@
+function [ HH ] = HheaderCreate( pacID, nick,channelsfile )
+%HHEADERCREATE vytvori header ve formatu Hammer Freiburg
+%   pacID = id pacienta, nick - zkracene prijmeni, channelsfile - soubor se jmeny kanalu
+HH = struct;
+HH.patientTag = pacID;
+HH.patientNick = nick;
+HH.channels = struct;
+assert(exist(channelsfile,'file')==2,'soubor se jmeny kanalu neexistuje');
+channelnames = importdata(channelsfile,','); %file, kde na kazdem radku je jedno jmeno kanalu napr A1 
+chnum = size(channelnames,1);
+for ch = 1:chnum  
+    tmp = regexp(channelnames{ch,1},'([^ ,:]*)','tokens'); %na radce v channels muze byt jako druhe jmeno struktury oddelene carkou
+    names = cat(2,tmp{:});
+    HH.channels(ch).name=names{1};
+    HH.channels(ch).numberOnAmplifier=ch;
+    if ch==chnum-2
+        signalType = 'triggerCh'; %vetsinou 2 pred koncem, kdyztak opravim rucne
+    elseif chnum-ch < 2
+        signalType = 'ECG'; %EKG
+    else
+        signalType = 'SEEG'; %iEEG channel
+    end
+    HH.channels(ch).signalType=signalType; 
+    HH.channels(ch).ass_brainAtlas='n.a.';
+    if ch <= 64
+        HH.channels(ch).headboxNumber=1; %tohle pripadne muze byt spatne
+    else 
+        HH.channels(ch).headboxNumber=2;
+    end
+    if(numel(names)>1)
+        HH.channels(ch).neurologyLabel=names{2};
+    else
+        HH.channels(ch).neurologyLabel='n.a.';
+    end
+end
+
+end
+
