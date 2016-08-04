96f198aa1f8113021c0ff81e83eb9e34f6719c30
KamilVlcek <kamil@biomed.cas.cz>
2016-08-04 16:15:27 +0200

    iEEG - CHHeader.mder - ChannelPlot - zobrazeni pozic elektrod
    - zobrazi 3D obrazek elektrod v MNI prostoru. Obrazek ma rozmery podle rozmeru mozku
    -- pohled muze urcti smer pohledu s-sagital,c-coronal,h-horizontal
    - upravy funkce ChannelGroups aby ukladala vysledky funkce getChannelGroups_kisarg, ktera trva docela dlouho
    
    - funkce anotacenajdi ted ingoruje soubory bez headeru
    -- ma take novy parametr filtr, ktery muze obsahovat cast jmena souboru
    - funkci readnifti jsem stahnul na zobrazeni MNI templatu, ale zatim nepouzil, protoze mi nejsou jasne souradnice (kde je 0?)

--- CHHeader.m
+++ CHHeader.m
@@ -5,6 +5,8 @@ classdef CHHeader < handle
     properties (Access = public)
         H; %data od Jirky Hammera
         %E; % unikatni jmena elektrod, napr RG, I, GC ...
+        chgroups;
+        els;
     end
     
     methods (Access = public)
@@ -26,12 +28,19 @@ classdef CHHeader < handle
         
         function [chgroups, els] = ChannelGroups(obj)
             %vraci skupiny kanalu (cisla vsech channels na elekrode) + cisla nejvyssiho kanalu v na kazde elektrode v poli els
-            chgroups = getChannelGroups_kisarg(obj.H,'perElectrode');
-            els = zeros(1,numel(chgroups));
-            for j = 1:numel(chgroups);
-                els(j)=max(chgroups{j});
+            if isempty(obj.chgroups)
+                chgroups = getChannelGroups_kisarg(obj.H,'perElectrode');
+                els = zeros(1,numel(chgroups));
+                for j = 1:numel(chgroups);
+                    els(j)=max(chgroups{j});
+                end
+                els = sort(els);
+                obj.chgroups = chgroups; 
+                obj.els = els;
+            else
+                chgroups = obj.chgroups; 
+                els = obj.els;
             end
-            els = sort(els);
         end
         function [ch,name,MNI,brainAtlas] = ChannelNameFind(obj,name)
             % najde kanal podle jmena kontaktu, napr 'R5', name musi byt na zacatku jmena, 
@@ -53,7 +62,47 @@ classdef CHHeader < handle
             end
             ch = 0;
         end
-            
+        function ChannelPlot(obj,pohled)
+            %zobrazi 3D obrazek elektrod v MNI prostoru. Obrazek ma rozmery podle rozmeru mozku
+            %pohled muze urcti smer pohledu s-sagital,c-coronal,h-horizontal
+            if isfield(obj.H.channels,'MNI_x')
+                figure('Name','ChannelPlot in MNI');                
+                chgroups = obj.ChannelGroups(); %#ok<PROP>
+                for chg = 1:size(chgroups,2) %#ok<PROP>
+                    group = chgroups{chg}; %#ok<PROP>
+                    X = zeros(1,numel(group)); Y = X; Z = X;
+                    for ich = 1:numel(group)                        
+                        X(ich) = obj.H.channels(group(ich)).MNI_x;
+                        Y(ich) = obj.H.channels(group(ich)).MNI_y;
+                        Z(ich) = obj.H.channels(group(ich)).MNI_z;                        
+                    end
+                    plot3(X,Y,Z,'o-','MarkerSize',5,'MarkerEdgeColor','b','MarkerFaceColor','b','LineWidth',2);
+                    if chg==1, hold on; end                    
+                    for ich = 1:numel(group) 
+                        text(X(ich),Y(ich),Z(ich)+3,obj.H.channels(group(ich)).name);
+                    end
+                end
+                xlabel('MNI X'); %levoprava souradnice
+                ylabel('MNI Y'); %predozadni souradnice
+                zlabel('MNI Z'); %hornodolni
+                if ~exist('pohled','var'), pohled = ''; end
+                switch pohled
+                    case 's' %sagital = levoprava
+                        view([-1 0 0]); %zleva
+                    case 'c' %coronal = predozadni
+                        view([0 1 0]); %zepredu
+                    case 'h' %horizontal = hornodolni   
+                        view([0 0 1]); %shora
+                end
+                axis([-75 75 -120 70 -50 90]); %zhruba velikost mozku        
+                text(-70,0,0,'LEVA');        
+                text(70,0,0,'PRAVA');   
+                text(0,65,0,'VPREDU');        
+                text(0,-115,0,'VZADU');   
+            else
+                disp('No MNI data');
+            end
+        end
        
     end
     
--- anotacenajdi.m
+++ anotacenajdi.m
@@ -1,12 +1,23 @@
-function []=anotacenajdi(adresar)
+function []=anotacenajdi(adresar,filter)
 % ANOTACENAJDI vypise anotace ze vsech souboru mat 
 % napriklad anotacenajdi('D:\eeg\motol\pacienti\Pluhackova\');
+% filter je retezec, ktery musi obsahovat jmeno souboru
 
+if ~exist('filter','var'), filter = []; end %pokud neni definovan filter, vytvorim jako prazdny
 files = dir(fullfile(adresar, '*.mat'));
 
 for f = 1:numel(files)
-    load([ adresar files(f).name],'header','tabs');
-    disp(['*** ' files(f).name]);
-    anotace(header,tabs);
-    clear header d tabs fs;
+    filename = [ adresar files(f).name];
+    if isempty(filter) || ~isempty(strfind(filename,filter))
+        %zpracovavam jen soubory vyhovujici filtru, pokud nejaky existuje
+        vars = whos('-file',filename);
+        if ismember('header', {vars.name})
+            load(filename,'header','tabs');
+            disp(['*** ' files(f).name]);
+            anotace(header,tabs);    
+            clear header d tabs fs;
+        else
+           disp(['*** ' files(f).name '- no header']);
+        end
+    end
 end
