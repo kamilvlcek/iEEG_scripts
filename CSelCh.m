4b74155b5050ba9df196abe2da363e6204f2c505
kamilvlcek <kamil@biomed.cas.cz>
2018-07-02 17:56:42 +0200

    iEEG - CSelCh - ziskavanni kanalu a jejich ukladani primo do objektu CM aj
    - ve funkcich SetSelCH a GetSelCh
    - konstruktorem se primo muze nacist soubor
    - funkce Save a Load nacitaji data tridy

--- CSelCh.m
+++ CSelCh.m
@@ -5,24 +5,100 @@ classdef CSelCh < matlab.mixin.Copyable
     properties (Access = public)
         selCh;
         n; %pocet ulozenych dat
+        filename;
     end
     
     methods (Access = public)
-        function obj = CSelCh()
-            obj.selCh = cell(1,4); %prvni sloupec je filename, druhy selCh, treti katstr, ctvrty freq 
-            obj.n = 0;
+        function obj = CSelCh(filename)
+            if exist('filename','var')                
+                obj.Load(filename);            
+            else
+                obj.selCh = cell(1,5); %prvni sloupec je filename, druhy selCh, treti katstr, ctvrty freq , paty chnames
+                obj.n = 0;
+            end
         end
-        function obj = SetSelCh(obj,selCh,filename,katstr,freq)            
+        function obj = SetSelCh(obj,selCh,filename,chnames, katstr,freq)            
             %ulozi vyber kanalu            
-            if ~exist('katstr','var'), katstr = ''; end
-            if ~exist('freq','var'), freq = ''; end
-            obj.selCh(obj.n+1,:) = {filename,selCh,katstr,freq};
-            obj.n = obj.n +1;
+            if ~exist('chnames','var'), chnames = []; end
+            if ~exist('katstr','var'), katstr = []; end
+            if ~exist('freq','var'), freq = []; end
+            if isa(selCh,'CiEEGData')
+                %muzu predat jako parametr celou tridu CM aj, data z nim ziskam sam
+                E = selCh;
+                selCh = E.GetSelCh();
+                filename = E.filename;
+                if isprop(E,'label')
+                    katstr = E.label; 
+                else
+                    katstr = []; 
+                end
+                if isprop(E, 'Hf') 
+                    freq = [num2str(E.Hf(1)) '-' num2str(E.Hf(end))];
+                else
+                    freq = [];
+                end
+                chnames = {E.CH.H.channels(selCh).name}; %jmena vybranych kanaly
+                obj.SetSelCh(selCh,filename,chnames,katstr,freq);
+            else
+                if obj.n>0
+                    s = find(~cellfun(@isempty,strfind(obj.selCh(:,1),filename)),1); %najdu pouze prvni vyhovujici soubor
+                else
+                    s = [];
+                end
+                if isempty(s) %filename neexistuje, ulozim
+                    obj.selCh(obj.n+1,:) = {filename,selCh,katstr,freq,chnames};
+                    obj.n = obj.n +1;
+                else %filename uz ulozen, radku s nim prepisu
+                    obj.selCh(s,:) = {filename,selCh,katstr,freq,chnames}; 
+                end
+                disp([ num2str(numel(selCh)) ' selected channels saved']);                   
+            end
         end
         function selCh = GetSelCh(obj,filename)
-            s = find(~cellfun(@isempty,strfind(obj.selCh(:,1),filename)),1); %najdu pouze prvni vyhovujici soubor
-            selCh = obj.selCh{s,2};
+            if isa(filename,'CiEEGData')
+                 E = filename;
+                 s = find(~cellfun(@isempty,strfind(obj.selCh(:,1),E.filename)),1); %najdu pouze prvni vyhovujici soubor 
+                 if ~isempty(s)
+                     selCh = obj.selCh{s,2};                     
+                     selChNames = obj.selCh{s,5};
+                     for ich = 1:numel(selCh)
+                        if ~isempty(obj.selCh{s,5})
+                            assert(strcmp(selChNames{ich},E.CH.H.channels(selCh(ich)).name),['channel ' num2str(selCh(ich)) ' nesedi s popisem: ' selChNames{ich} ' vs.' E.CH.H.channels(selCh(ich)).name]);                                                        
+                        end
+                     end
+                     E.plotRCh.selCh = selCh; %vlozim cisla kanalu do objektu, tam kam patri
+                     disp('ulozeno do vlastnosti tridy');
+                 else
+                     disp('soubor nenalezen v obj.selCh');
+                 end                     
+            else
+                s = find(~cellfun(@isempty,strfind(obj.selCh(:,1),filename)),1); %najdu pouze prvni vyhovujici soubor
+                if ~isempty(s)
+                    selCh = obj.selCh{s,2};
+                else
+                    disp('soubor nenalezen v obj.selCh');   
+                    selCh = [];
+                end
+            end
         end
+        function Save(obj,filename)
+            if ~exist('filename','var') 
+                filename = obj.filename;
+            end
+            selCh = obj.selCh; %#ok<NASGU,PROPLC>
+            n = obj.n;      %#ok<NASGU,PROPLC>      
+            save(filename,'selCh','n','filename','-v7.3');  
+            disp(['ulozeno do ' filename]); 
+        end
+        function obj = Load(obj,filename)            
+            assert(exist(filename,'file')==2, 'soubor neexistuje');
+            load(filename,'selCh','n','filename');              
+            obj.n = n; %#ok<CPROPLC>
+            obj.selCh = selCh; %#ok<CPROPLC>
+            obj.filename = filename;
+            disp(['nacten soubor ' filename]); 
+        end
+            
     end
     
 end
