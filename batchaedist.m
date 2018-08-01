2b2548ab0960fae9ffd9fd2366881f453dd361bc
kamilvlcek <kamil@biomed.cas.cz>
2018-08-01 15:56:11 +0200

    iEEG - batchappa, batchaedist, pacienti_ppa - opravy
    - jednotny format batchppa a batchaedist s konfiguraci tri casti na zacatku
    - serazeni pacientu a oprava jednoho jmena v pacienti_ppa

--- batchaedist.m
+++ batchaedist.m
@@ -1,19 +1,28 @@
-%% nejdriv normalni analyzu s razenim podle podnetu
-disp(' ++++ ANALYZA 1 - RAZENI PODLE PODNETU ++++');
-%pacienti = {'p099'}; 
-cfg = struct('hybernovat',0,'suffix','Ep2018-07'); %'Ep2018-04', 'Ep2018-06'
-%cfg.pacienti = pacienti; %kdyz to tam vlozim rovnou, tak se mi udela struct array
-%cfg.overwrite=1; %vyjimecne
-%cfg.freqepochs=1; %vyjimecne
-BatchHilbert('aedist',cfg);
-return; %nic dalsiho zatim nechci 
-%% potom analyza s razenim podle odpovedi
-disp(' ++++ ANALYZA 2 - RAZENI PODLE ODPOVEDI ++++');
-cfg = struct('hybernovat',0,'srovnejresp',1);
-BatchHilbert('aedist',cfg);
+%% ktere analyzy chci spustit
+podnet = 1;
+odpovedi = 0;
+podilcasuodpovedi =0;
 
+%% nejdriv normalni analyzu s razenim podle podnetu
+ if podnet
+    disp(' ++++ ANALYZA 1 - RAZENI PODLE PODNETU ++++');
+    %pacienti = {'p099'}; 
+    cfg = struct('hybernovat',0,'suffix','Ep2018-07'); %'Ep2018-04', 'Ep2018-06'
+    %cfg.pacienti = pacienti; %kdyz to tam vlozim rovnou, tak se mi udela struct array
+    %cfg.overwrite=1; %vyjimecne
+    %cfg.freqepochs=1; %vyjimecne
+    filenames = BatchHilbert('aedist',cfg);
+end
+%% potom analyza s razenim podle odpovedi
+if odpovedi
+    disp(' ++++ ANALYZA 2 - RAZENI PODLE ODPOVEDI ++++');
+    cfg = struct('hybernovat',0,'srovnejresp',1);
+    filenames = BatchHilbert('aedist',cfg);
+end
 %% nakonec analyza s podilem casu odpovedi
 % uz budu na konci hybernovat
-disp(' ++++ ANALYZA 3 - PODIL CASU ODPOVEDI ++++');
-cfg = struct('hybernovat',1,'podilcasuodpovedi',1);
-BatchHilbert('aedist',cfg);
+if podilcasuodpovedi
+    disp(' ++++ ANALYZA 3 - PODIL CASU ODPOVEDI ++++');
+    cfg = struct('hybernovat',1,'podilcasuodpovedi',1);
+    filenames = BatchHilbert('aedist',cfg);
+end
--- batchappa.m
+++ batchappa.m
@@ -1,19 +1,28 @@
-%% nejdriv normalni analyzu s razenim podle podnetu
-disp(' ++++ ANALYZA 1 - RAZENI PODLE PODNETU ++++');
-%pacienti = {'p099'}; 
-cfg = struct('hybernovat',0,'suffix','Ep2018-08'); %'Ep2018-04', 'Ep2018-06'
-%cfg.pacienti = pacienti; %kdyz to tam vlozim rovnou, tak se mi udela struct array
-%cfg.overwrite=1; %vyjimecne
-%cfg.freqepochs=1; %vyjimecne
-BatchHilbert('ppa',cfg);
-return; %nic dalsiho zatim nechci 
-%% potom analyza s razenim podle odpovedi
-disp(' ++++ ANALYZA 2 - RAZENI PODLE ODPOVEDI ++++');
-cfg = struct('hybernovat',0,'srovnejresp',1);
-BatchHilbert('ppa',cfg);
+%% ktere analyzy chci spustit
+podnet = 1;
+odpovedi = 0;
+podilcasuodpovedi =0;
 
+%% nejdriv normalni analyzu s razenim podle podnetu
+if podnet
+    disp(' ++++ ANALYZA 1 - RAZENI PODLE PODNETU ++++');
+    %pacienti = {'p082'}; 
+    cfg = struct('hybernovat',0,'suffix','Ep2018-08');
+    %cfg.pacienti = pacienti; %kdyz to tam vlozim rovnou, tak se mi udela struct array
+    %cfg.overwrite=1; %vyjimecne
+    filenames = BatchHilbert('ppa',cfg);    
+end
+%% potom analyza s razenim podle odpovedi
+if odpovedi
+    disp(' ++++ ANALYZA 2 - RAZENI PODLE ODPOVEDI ++++');
+    cfg = struct('hybernovat',0,'srovnejresp',1,'suffix','Ep2018-07'); %,'suffix','Ep2018-01'
+    cfg.overwrite=0; %vyjimecne
+    filenames = BatchHilbert('ppa',cfg);
+end
 %% nakonec analyza s podilem casu odpovedi
 % uz budu na konci hybernovat
-disp(' ++++ ANALYZA 3 - PODIL CASU ODPOVEDI ++++');
-cfg = struct('hybernovat',1,'podilcasuodpovedi',1);
-BatchHilbert('ppa',cfg);
+if podilcasuodpovedi
+    disp(' ++++ ANALYZA 3 - PODIL CASU ODPOVEDI ++++');
+    cfg = struct('hybernovat',0,'podilcasuodpovedi',1); %,'suffix','Ep2018-01'
+    filenames = BatchHilbert('ppa',cfg);
+end
