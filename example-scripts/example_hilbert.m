0ed28a634dce5ae9f89aca206e6552934d251e98
hejtmy <hejtmy@gmail.com>
2020-01-07 01:58:03 +0100

    renames the example script

--- example-scripts/example_hilbert.m
+++ example-scripts/example_hilbert.m
@@ -1,7 +1,6 @@
 patient_file = '..\example-data\p073_PPA CHilbert 50-150Hz -0.2-0.8 refBipo Ep2019-11 FEX_CHilb.mat';
 
 open(patient_file)
-% Notes
 % file has no information about referencing
 % Stats are run immediately after initialization - not recommended
 hilbert = CHilbert(patient_file);
