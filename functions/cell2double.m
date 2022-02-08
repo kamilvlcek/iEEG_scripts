bfb7bbaae236d7ea08ef1afd8cd038d5842a7376
kamilvlcek <kamil@biomed.cas.cz>
2022-02-08 14:41:06 +0100

    CiEEGData,CPsyData, CHilbertMulti - several corrections for HFreqEpochs , trialtypes and combined categories
    CiEEGData - GetKatsNames,IntervalyResp,PlotResponseCh-  minor corrections for trialtypes and  combined categories
    cell2double - added funtionality to explode numerical vectors in cell to main vector, similarly to cell2mat.
    		But accepts also numerical vector, then does nothing
    CPsyData - ReactionTime - correction for combined categories, when 'kategories' is cell containing vectors of categories
    CHilbertMulti - FindExtract - the label can be also empty to work as '*'
    	- ImportExtract - correction of usage of which parameter

--- functions/cell2double.m
+++ functions/cell2double.m
@@ -1,15 +1,32 @@
 function [b] = cell2double(a)
 %CELL2DOUBLE converts cell array with intermixed double values and char arrays to double array
-%   Detailed explanation goes here
+%   also if cell contains multiple double vectors, if explodes them to the main vector, similarly to cell2mat
+%   if a is not a cell, it returns the same value
 if iscell(a)
-    b = zeros(size(a));
+    numa = 0;
     for ia = 1:numel(a)
-        if ischar(a{ia}) || isstring(a{ia})
-            b(ia) = str2double(a{ia});
+        if isnumeric(a{ia})
+            numa = numa + numel(a{ia});
         else
-            b(ia) = a{ia};
+            numa = numa + 1;
         end
     end
+    b = iff(isrow(a),zeros(1,numa),zeros(numa,1));
+    ib = 1;
+    for ia = 1:numel(a)
+        if ischar(a{ia}) || isstring(a{ia})
+            b(ib) = str2double(a{ia});
+            ib = ib + 1;
+        elseif isnumeric(a{ia})
+            b(ib:ib+numel(a{ia})-1) = a{ia};
+            ib = ib + numel(a{ia});
+        else
+            b(ib) = a{ia};
+            ib = ib + 1;
+        end
+    end
+else
+    b = a;
 end
 end
 
